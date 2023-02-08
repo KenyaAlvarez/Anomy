751a51fab91c3537d4f879c69974736d2955bbec
Kenya <kalvarez@orthocana.com>
2023-02-07 20:52:25 -0600

    Added to constants and packages SQL.

---
+++ Anomy/Constants/AnomyConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anomy.Constants
+{
+    public static class AnomyConstants
+    {
+        public const string DataBaseFileName = "";
+
+        public const SQLite.SQLiteOpenFlags Flags = SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DataBaseFileName);
+            }
+        }
+    }
+}
